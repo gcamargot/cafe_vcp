73a5679478cd8daf19e6066bc1106841e22fab7e
gcamargot <gastonmatiascamargo@gmail.com>
2024-12-23 01:53:26 -0300

    fix: added proper import to avoid errors

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .auth.router import router as auth_router
 from .tables.router import router as tables_router
+from .products.router import router as products_router
+
 
 app = FastAPI(title="Café System API")
 
@@ -17,6 +19,7 @@ app.add_middleware(
 # Incluir routers
 app.include_router(auth_router)
 app.include_router(tables_router)
+app.include_router(products_router)
 
 
 @app.get("/")
